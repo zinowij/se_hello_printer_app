36b8e6ae64764dad119653ef7355617a2700208f
zinowij <zinowij@users.noreply.github.com>
2021-04-25 12:10:08 +0000

    update Makefile and Formater

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -28,8 +28,10 @@ def format_to_json(msg, imie):
     return ('{ "imie":"' + imie + '", "mgs":"' +
             msg + '"}')
 
+
 def format_to_xml(msg, imie):
-    return ('<greetings> <name>' + imie + '</name> <msg>' + msg + '</msg> </greetings>')
+    return ('<greetings> <name>' + imie + '</name> <msg>' +
+            msg + '</msg> </greetings>')
 
 
 def plain_text(msg, imie):
